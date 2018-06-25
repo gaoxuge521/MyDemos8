d4967a2ab283d25efaeab13469c966c302f206c1
gaoxuge521 <android_gaoxuge@163.com>
2018-06-25 18:05:02 +0800

    信鸽推送，华为通道有问题

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -164,3 +164,21 @@
 -keep class com.google.gson.stream.** { *; }
 -keepattributes EnclosingMethod
 -keep class com.gxg.demo8.mydemo8.rxjava_retrofit_okhttp.bean.**{*;}
+
+ # 信鸽
+ -keep public class * extends android.app.Service
+ -keep public class * extends android.content.BroadcastReceiver
+ -keep class com.tencent.android.tpush.** {* ;}
+ -keep class com.tencent.mid.** {* ;}
+ -keep class com.qq.taf.jce.** {*;}
+
+  #华为推送
+  -ignorewarning
+  -keepattributes *Annotation*
+  -keepattributes Exceptions
+  -keepattributes InnerClasses
+  -keepattributes Signature
+  -keepattributes SourceFile,LineNumberTable
+  -keep class com.hianalytics.android.**{*;}
+  -keep class com.huawei.updatesdk.**{*;}
+  -keep class com.huawei.hms.**{*;}
--- app/src/main/java/com/gxg/demo8/mydemo8/tinker/MyApplicationLike.java
+++ app/src/main/java/com/gxg/demo8/mydemo8/tinker/MyApplicationLike.java
@@ -4,8 +4,12 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.support.multidex.MultiDex;
+import android.util.Log;
 
 import com.socks.library.KLog;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 import com.tencent.tinker.anno.DefaultLifeCycle;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.app.ApplicationLike;
@@ -13,11 +17,10 @@ import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by gaoxuge on 2017/11/2.
- *
  */
 @DefaultLifeCycle(application = ".MyApp",//而这个类是真正的自定义的application,别忘了在AndroidManifest.xml中使用
         flags = ShareConstants.TINKER_ENABLE_ALL,
-        loadVerifyFlag =true)
+        loadVerifyFlag = true)
 public class MyApplicationLike extends ApplicationLike {
     public MyApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
@@ -30,9 +33,28 @@ public class MyApplicationLike extends ApplicationLike {
         TinkerInstaller.install(this);
 
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
         KLog.e("sss 初始化");
+        XGPushConfig.enableDebug(getApplication(), true);
+        //打开第三方推送
+        XGPushConfig.enableOtherPush(getApplication(), true);
+
+        XGPushManager.registerPush(getApplication(), new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+            //token在设备卸载重装的时候有可能会变
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
+
+
     }
 }
